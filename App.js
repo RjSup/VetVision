da847b30dd5aa86d6e4326bb6391171b6a60a87b
Ryan J <19141558@brookes.ac.uk>
2023-12-08 00:41:12 +0000

    Initial set up Vet Vision

--- App.js
+++ App.js
@@ -1,35 +1,61 @@
 // add firebase services
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    projectId: 'YOUR_PROJECT_ID',
-    appId: 'YOUR_APP_ID',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    databaseURL: 'YOUR_DATABASE_URL',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
+    apiKey: "AIzaSyB8r1TwfZib9PcYoWYET0XHHAthY5QZIj4",
+    authDomain: "vet-vision.firebaseapp.com",
+    projectId: "vet-vision",
+    storageBucket: "vet-vision.appspot.com",
+    messagingSenderId: "961984954386",
+    appId: "1:961984954386:web:781e80b56c618659fb98cb"
 };
 
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
-initializeApp(firebaseConfig);
+// Sign-in function
+function signIn() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-const analytics = getAnalytics();
-analytics.logEvent = function (userSignIn) {
-    
-};
-analytics.logEvent('User Sign-in');
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            // Sign-in successful, show user information
+            showUserInformation();
+        })
+        .catch((error) => {
+            // Sign-in failed, display error message
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(`Error signing in: ${errorCode} - ${errorMessage}`);
+        });
+}
 
-import { auth } from 'firebase/auth';
+// Sign-up function
+function signup() {
+    const name = document.getElementById('name').value;
+    const signupEmail = document.getElementById('signupEmail').value;
 
-const auth = auth();
 
-const email = 'user@example.com';
-const password = 'password123';
+    const signupPassword = document.getElementById('signupPassword').value;
 
-auth.createUserWithEmailAndPassword = function (email, password) {
-
-};
-auth.createUserWithEmailAndPassword(email, password)
-    .then(user => console.log('User signed up:', user))
-    .catch(error => console.error('Error signing up user:', error));
+    auth.createUserWithEmailAndPassword(signupEmail, signupPassword)
+        .then(user => {
+            // User created successfully, add name to profile
+            user.updateProfile({ displayName: name })
+                .then(() => {
+                    // Update complete, show user information
+                    showUserInformation();
+                })
+                .catch((error) => {
+                    // Update failed, display error message
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(`Error updating profile: ${errorCode} - ${errorMessage}`);
+                });
+        })
+        .catch((error))}
